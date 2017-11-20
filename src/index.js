df41150f4b5e5cafab2149313e1739af3abab297
Wyatt Cready <jcready@gmail.com>
2017-11-20 14:16:16 -0500

    Make all polymorphic parameters covariant

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 // @flow
 
-export type Result<Data, Error> =
+export type Result<+Data, +Error> =
     {| tag: 'Ok', +data: Data |}
   | {| tag: 'Err', +err: Error |}
 
-export const Ok = <Data, Error>(data: Data) : Result<Data, Error> => ({ tag: 'Ok', data })
-export const Err = <Data, Error>(err: Error) : Result<Data, Error> => ({ tag: 'Err', err })
+export const Ok = <+Data, +Error>(data: Data) : Result<Data, Error> => ({ tag: 'Ok', data })
+export const Err = <+Data, +Error>(err: Error) : Result<Data, Error> => ({ tag: 'Err', err })
 
-export const andThen = <Data, NewData, Error>(result: Result<Data, Error>, f: Data => Result<NewData, Error>) : Result<NewData, Error> => {
+export const andThen = <+Data, +NewData, +Error>(result: Result<Data, Error>, f: Data => Result<NewData, Error>) : Result<NewData, Error> => {
   if (result.tag === 'Ok') {
     return f(result.data)
   } else {
@@ -15,7 +15,7 @@ export const andThen = <Data, NewData, Error>(result: Result<Data, Error>, f: Da
   }
 }
 
-export const mapOk = <Data, Error, NewData>(result: Result<Data, Error>, f: Data => NewData) : Result<NewData, Error> => {
+export const mapOk = <+Data, +Error, +NewData>(result: Result<Data, Error>, f: Data => NewData) : Result<NewData, Error> => {
   if (result.tag === 'Ok') {
     return Ok(f(result.data))
   } else {
@@ -23,7 +23,7 @@ export const mapOk = <Data, Error, NewData>(result: Result<Data, Error>, f: Data
   }
 }
 
-export const mapErr = <Data, NewError, Error>(result: Result<Data, Error>, f: Error => NewError) : Result<Data, NewError> => {
+export const mapErr = <+Data, +NewError, +Error>(result: Result<Data, Error>, f: Error => NewError) : Result<Data, NewError> => {
   if (result.tag === 'Ok') {
     return Ok(result.data)
   } else {
@@ -31,7 +31,7 @@ export const mapErr = <Data, NewError, Error>(result: Result<Data, Error>, f: Er
   }
 }
 
-export const unwrapOrElse = <Data, Error>(result: Result<Data, Error>, f: Error => Data) : Data => {
+export const unwrapOrElse = <+Data, +Error>(result: Result<Data, Error>, f: Error => Data) : Data => {
   if (result.tag === 'Ok') {
     return result.data
   } else {
@@ -39,7 +39,7 @@ export const unwrapOrElse = <Data, Error>(result: Result<Data, Error>, f: Error 
   }
 }
 
-export const collectResultMap = <MapVal, Data, Error>(oldMap: { [key: string]: MapVal }, f: (string, MapVal) => Result<Data, Error>) : Result<{ [key: string]: Data }, Error> => {
+export const collectResultMap = <+MapVal, +Data, +Error>(oldMap: { [key: string]: MapVal }, f: (string, MapVal) => Result<Data, Error>) : Result<{ [key: string]: Data }, Error> => {
   const newMap: { [key: string]: Data } = {}
   for (const label in oldMap) {
     const oldValue = oldMap[label]
@@ -53,7 +53,7 @@ export const collectResultMap = <MapVal, Data, Error>(oldMap: { [key: string]: M
   return Ok(newMap)
 }
 
-export const collectResultArray = <ArrayVal, Data, Error>(oldArray: ArrayVal[], f: ArrayVal => Result<Data, Error>) : Result<Data[], Error> => {
+export const collectResultArray = <+ArrayVal, +Data, +Error>(oldArray: ArrayVal[], f: ArrayVal => Result<Data, Error>) : Result<Data[], Error> => {
   const newArray: Data[] = []
   for (const oldValue of oldArray) {
     const newValue = f(oldValue)
@@ -66,7 +66,7 @@ export const collectResultArray = <ArrayVal, Data, Error>(oldArray: ArrayVal[], 
   return Ok(newArray)
 }
 
-export const collectResultArrayIndexed = <ArrayVal, Data, Error>(
+export const collectResultArrayIndexed = <+ArrayVal, +Data, +Error>(
   oldArray: ArrayVal[],
   f: (index: number, val: ArrayVal) => Result<Data, Error>
 ) : Result<Data[], Error> => {
